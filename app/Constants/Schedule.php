bbfa883d20c5014114e90525e99370238b0a5c97
juanco <juancoloradoalvarez@gmail.com>
2021-10-16 00:29:26 -0500

    feat: replace constants into the test and some Model method

---
+++ app/Constants/Schedule.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Constants;
+
+class Schedule
+{
+    public const MINUTE = 'minute';
+    public const HOUR = 'hour';
+    public const DAY_MONTH = 'day_month';
+    public const MONTH = 'month';
+    public const DAY_WEEK = 'day_week';
+    public const YEAR = 'year';
+}
