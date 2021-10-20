c886e174b0573d69b07dcd354e85a286704514ec
juanco <juancoloradoalvarez@gmail.com>
2021-10-19 22:59:42 -0500

    feat: AbastractSpecification Class for Specification Pattern
    here we will set the Specification Defautl to evaluate into the isSatisfyBy metodh and inicializate some attributes of the class.

---
+++ app/Scheduler/Specifications/AbstractSpecification.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Scheduler\Specifications;
+
+use App\Constants\Schedule as ScheduleConstant;
+use Carbon\Carbon;
+use App\Models\Schedule;
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\Validator;
+use App\Scheduler\Contracts\Specification;
+
+abstract class AbstractSpecification implements Specification
+{
+    protected Schedule $schedule;
+    protected $minute;
+    protected $hour;
+    protected $month;
+    protected $day_month;
+    protected $day_week;
+
+    public function __construct(Schedule $schedule)
+    {
+        $this->schedule = $schedule;
+        $this->minute = Carbon::now()->format('i');
+        $this->hour = CarbonImmutable::now()->isoFormat('H');
+        $this->month = CarbonImmutable::now()->isoFormat('M');
+        $this->day_month = CarbonImmutable::now()->isoFormat('D');
+        $this->day_week = CarbonImmutable::now()->weekday();
+
+    }
+
+    public function isSatisfyBy(string $minuteType, string $hourType, string $dayMonthType, string $monthType, string $dayWeekType): bool
+    {
+        $validate = Validator::make($this->schedule->getAttributes(), [
+            ScheduleConstant::MINUTE => $minuteType,
+            ScheduleConstant::HOUR => $hourType,
+            ScheduleConstant::DAY_MONTH => $dayMonthType,
+            ScheduleConstant::MONTH => $monthType,
+            ScheduleConstant::DAY_WEEK => $dayWeekType,
+        ]);
+
+        return !$validate->fails();
+    }
+
+    public function asQuery(): bool
+    {
+    }
+}
