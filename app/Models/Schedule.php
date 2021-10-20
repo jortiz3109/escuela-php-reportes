2aff39208fad499680c8eb77a24b99647180ffd2
juanco <juancoloradoalvarez@gmail.com>
2021-10-19 23:05:20 -0500

    fix: update Schedule Model to implement the Specification Pattern

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -2,26 +2,13 @@
 
 namespace App\Models;
 
-use App\Constants\Schedule as Constants;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Schedule extends Model
 {
-
     use HasFactory;
 
     public $timestamps = false;
 
-    public function scopeReportsToSchedule(Builder $query, array $date): Builder
-    {
-        $query = Schedule::select('report_id')
-            ->where(Constants::MINUTE, $date[Constants::MINUTE])
-            ->orWhere(Constants::HOUR,  $date[Constants::HOUR])
-            ->orWhere(Constants::DAY_MONTH,  $date[Constants::DAY_MONTH])
-            ->orWhere(Constants::MONTH,  $date[Constants::MONTH])
-            ->orWhere(Constants::DAY_WEEK,  $date[Constants::DAY_WEEK]);
-        return $query;
-    }
 }
