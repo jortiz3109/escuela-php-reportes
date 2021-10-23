e4a980659cf26ec6c86e280c7d89af876df9738d
Juan Ricardo Cardona <jucardonaa@unal.edu.co>
2021-10-23 12:08:54 -0500

    test: code duplicated correction

--- tests/Concerns/HasExportProviders.php
+++ tests/Concerns/HasExportProviders.php
@@ -11,20 +11,7 @@ trait HasExportProviders
         return [
             'get reports with xlsx' => [
                 'extension' => 'xlsx',
-                'filters' => [
-                    $this->makeFilter(null, 'transactions', 'reference'),
-                    $this->makeFilter(null, 'transactions', 'purchase_amount'),
-                    $this->makeFilter(null, 'transactions', 'platform_amount'),
-                    $this->makeFilter(null, 'transactions', 'truncated_pan'),
-                    $this->makeFilter(null, 'transactions', 'status'),
-                    $this->makeFilter(null, 'transactions', 'ip'),
-                    $this->makeFilter(null, 'transactions', 'created_at'),
-                    $this->makeFilter(null, 'currencies', 'alphabetic_code'),
-                    $this->makeFilter(null, 'merchants', 'name'),
-                    $this->makeFilter(null, 'countries', 'alpha_3_code'),
-                    $this->makeFilter(null, 'payers', 'name'),
-                    $this->makeFilter(null, 'payers', 'email'),
-                ],
+                'filters' => $this->allFields(),
             ],
         ];
     }
@@ -34,20 +21,7 @@ trait HasExportProviders
         return [
             'get reports with csv' => [
                 'extension' => 'csv',
-                'filters' => [
-                    $this->makeFilter(null, 'transactions', 'reference'),
-                    $this->makeFilter(null, 'transactions', 'purchase_amount'),
-                    $this->makeFilter(null, 'transactions', 'platform_amount'),
-                    $this->makeFilter(null, 'transactions', 'truncated_pan'),
-                    $this->makeFilter(null, 'transactions', 'status'),
-                    $this->makeFilter(null, 'transactions', 'ip'),
-                    $this->makeFilter(null, 'transactions', 'created_at'),
-                    $this->makeFilter(null, 'currencies', 'alphabetic_code'),
-                    $this->makeFilter(null, 'merchants', 'name'),
-                    $this->makeFilter(null, 'countries', 'alpha_3_code'),
-                    $this->makeFilter(null, 'payers', 'name'),
-                    $this->makeFilter(null, 'payers', 'email'),
-                ],
+                'filters' => $this->allFields(),
             ],
         ];
     }
@@ -57,20 +31,7 @@ trait HasExportProviders
         return [
             'get reports with tsv' => [
                 'extension' => 'tsv',
-                'filters' => [
-                    $this->makeFilter(null, 'transactions', 'reference'),
-                    $this->makeFilter(null, 'transactions', 'purchase_amount'),
-                    $this->makeFilter(null, 'transactions', 'platform_amount'),
-                    $this->makeFilter(null, 'transactions', 'truncated_pan'),
-                    $this->makeFilter(null, 'transactions', 'status'),
-                    $this->makeFilter(null, 'transactions', 'ip'),
-                    $this->makeFilter(null, 'transactions', 'created_at'),
-                    $this->makeFilter(null, 'currencies', 'alphabetic_code'),
-                    $this->makeFilter(null, 'merchants', 'name'),
-                    $this->makeFilter(null, 'countries', 'alpha_3_code'),
-                    $this->makeFilter(null, 'payers', 'name'),
-                    $this->makeFilter(null, 'payers', 'email'),
-                ],
+                'filters' => $this->allFields(),
             ],
         ];
     }
--- tests/Concerns/HasFiltersProvider.php
+++ tests/Concerns/HasFiltersProvider.php
@@ -13,4 +13,22 @@ trait HasFiltersProvider
             'value' => $value,
         ];
     }
+
+    public function allFields(): array
+    {
+        return [
+            $this->makeFilter(null, 'transactions', 'reference'),
+            $this->makeFilter(null, 'transactions', 'purchase_amount'),
+            $this->makeFilter(null, 'transactions', 'platform_amount'),
+            $this->makeFilter(null, 'transactions', 'truncated_pan'),
+            $this->makeFilter(null, 'transactions', 'status'),
+            $this->makeFilter(null, 'transactions', 'ip'),
+            $this->makeFilter(null, 'transactions', 'created_at'),
+            $this->makeFilter(null, 'currencies', 'alphabetic_code'),
+            $this->makeFilter(null, 'merchants', 'name'),
+            $this->makeFilter(null, 'countries', 'alpha_3_code'),
+            $this->makeFilter(null, 'payers', 'name'),
+            $this->makeFilter(null, 'payers', 'email'),
+        ];
+    }
 }
