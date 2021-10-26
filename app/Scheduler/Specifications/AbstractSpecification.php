2e4a4b5101718cc19c8089d935f08bb30edc9e34
juanco <juancoloradoalvarez@gmail.com>
2021-10-26 09:20:34 -0500

    feat: use the SetDateTrait

--- app/Scheduler/Specifications/AbstractSpecification.php
+++ app/Scheduler/Specifications/AbstractSpecification.php
@@ -3,6 +3,7 @@
 namespace App\Scheduler\Specifications;
 
 use App\Constants\Schedule as ScheduleConstant;
+use App\Scheduler\Traits\SetCurrentDateTrait;
 use Carbon\Carbon;
 use App\Models\Schedule;
 use Carbon\CarbonImmutable;
@@ -11,26 +12,17 @@ use App\Scheduler\Contracts\Specification;
 
 abstract class AbstractSpecification implements Specification
 {
+    use SetCurrentDateTrait;
     protected Schedule $schedule;
-    protected $minute;
-    protected $hour;
-    protected $month;
-    protected $dayMonth;
-    protected $dayWeek;
 
     public function __construct(Schedule $schedule)
     {
         $this->schedule = $schedule;
-        $this->minute = Carbon::now()->format('i');
-        $this->hour = CarbonImmutable::now()->isoFormat('H');
-        $this->month = CarbonImmutable::now()->isoFormat('M');
-        $this->dayMonth = CarbonImmutable::now()->isoFormat('D');
-        $this->dayWeek = CarbonImmutable::now()->weekday();
-
     }
 
     public function isSatisfyBy(string $minuteType, string $hourType, string $dayMonthType, string $monthType, string $dayWeekType): bool
     {
+        $this->setDate($this);
         $validate = Validator::make($this->schedule->getAttributes(), [
             ScheduleConstant::MINUTE => $minuteType,
             ScheduleConstant::HOUR => $hourType,
