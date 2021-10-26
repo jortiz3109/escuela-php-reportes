2881d34169ce50ee6f785434f148cbd0049ce0b6
juanco <juancoloradoalvarez@gmail.com>
2021-10-26 09:09:02 -0500

    test: unit test for Scheduler Class

---
+++ tests/Unit/Scheduler/SchedulerTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Unit\Scheduler;
+
+use App\Constants\Schedule as ScheduleConstant;
+use App\Models\Report;
+use App\Models\Schedule;
+use App\Scheduler\Scheduler;
+use Tests\TestCase;
+use App\Scheduler\Traits\SetCurrentDateTrait;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SchedulerTest extends TestCase
+{
+    use RefreshDatabase;
+    use SetCurrentDateTrait;
+
+    protected Report $report;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->setDate($this);
+        $this->report = Report::create();
+        Schedule::factory()
+            ->create([
+                ScheduleConstant::MINUTE => $this->minute,
+                ScheduleConstant::HOUR => $this->hour,
+                ScheduleConstant::DAY_MONTH => $this->dayMonth,
+                ScheduleConstant::MONTH => $this->month,
+                ScheduleConstant::DAY_WEEK => $this->dayWeek,
+                'report_id' => $this->report->id
+            ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_run_the_specific_scheduled_reports()
+    {
+        $scheduler = new Scheduler();
+        $scheduler->builtReports();
+        $this->expectOutputString('A report with id ' . $this->report->id . ' was created');
+    }
+}
