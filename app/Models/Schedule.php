1565672f686397c3eb1e8e63fe281be1e7fa2245
juanco <juancoloradoalvarez@gmail.com>
2021-10-26 09:17:00 -0500

    feat: cache the current scheduled reports day for scheduler

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Constants\Schedule as ScheduleConstant;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -9,6 +11,28 @@ class Schedule extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+      'minute',
+      'hour',
+      'day_month',
+      'month',
+      'day_week',
+      'report_id'
+    ];
+
     public $timestamps = false;
 
+    public static function cacheHourlySchedule(string $dayMonth, string $month, string $dayWeek, string $cacheKey)
+    {
+        if (Cache::has($cacheKey)){
+            Cache::forget($cacheKey);
+        }
+
+        Cache::remember($cacheKey, 60, function () use ($dayMonth, $month, $dayWeek){
+            return Schedule::whereIn(ScheduleConstant::DAY_MONTH, [$dayMonth, '*'])
+            ->whereIn(ScheduleConstant::MONTH, [$month, '*'])
+            ->whereIn(ScheduleConstant::DAY_WEEK, [$dayWeek, '*'])
+            ->get();
+        });
+    }
 }
