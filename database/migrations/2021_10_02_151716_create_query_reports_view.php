bd78d78acfb73a13a27814e8f9678118eb7d0420
juanco <juancoloradoalvarez@gmail.com>
2021-10-26 12:58:36 -0500

    fix: database path to create the query report view

--- database/migrations/2021_10_02_151716_create_query_reports_view.php
+++ database/migrations/2021_10_02_151716_create_query_reports_view.php
@@ -13,7 +13,7 @@ class CreateQueryReportsView extends Migration
      */
     public function up()
     {
-        DB::unprepared(File::get('database/sql/CreateQueryReportsView.sql'));
+        DB::unprepared(File::get(database_path('sql/CreateQueryReportsView.sql')));
     }
 
     /**
@@ -23,6 +23,6 @@ class CreateQueryReportsView extends Migration
      */
     public function down()
     {
-        DB::unprepared(File::get('database/sql/DeleteQueryReportsView.sql'));
+        DB::unprepared(File::get(database_path('sql/DeleteQueryReportsView.sql')));
     }
 }
