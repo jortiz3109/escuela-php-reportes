ff202107b2c8514349b6e147b1aa51c5d646e677
juanco <juancoloradoalvarez@gmail.com>
2021-11-02 13:37:07 -0500

    refactor: change the builtReports and add scheduledDailyReport

--- app/Scheduler/Scheduler.php
+++ app/Scheduler/Scheduler.php
@@ -2,50 +2,35 @@
 
 namespace App\Scheduler;
 
-use App\Models\Schedule;
-use App\Scheduler\Specifications\HasMinutesHours;
-use App\Scheduler\Specifications\HasMinutesHoursDays;
-use App\Scheduler\Specifications\HasMinutesHoursDaysMonth;
-use App\Scheduler\Specifications\HasMinutesHoursDaysMonthWeek;
-use App\Scheduler\Traits\SetCurrentDateTrait;
+use App\Jobs\CreateReportJob;
+use App\Scheduler\Specifications\CronReportShouldBeRun;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class Scheduler
 {
-    use SetCurrentDateTrait;
-
-    public const REPORT_CLASS = [
-        1 => HasMinutesHours::class,
-        2 => HasMinutesHoursDays::class,
-        3 => HasMinutesHoursDaysMonth::class,
-        4 => HasMinutesHoursDaysMonthWeek::class,
-    ];
-
-    public const CACHE_KEY = 'scheduledHourlyReports';
-
     public function builtReports(): void
     {
-        $this->setDate();
-        Schedule::cacheDailyScheduledReports($this->dayMonth, $this->month, $this->dayWeek, self::CACHE_KEY);
-        $schedules = Cache::get(self::CACHE_KEY);
-
+        $schedules = $this->scheduledDailyReport();
         foreach ($schedules as $schedule) {
-            if ($this->validateSpecifications($schedule) === true) {
-                print_r('A report with id ' . $schedule->report_id . ' was created');
+            $specification = new CronReportShouldBeRun($schedule);
+            if ($specification->shouldExecuteReport()) {
+                CreateReportJob::dispatch($schedule);
+                Log::notice('A a job for report creation were dispatched, the report id was: ' . $schedule->report_id);
             }
-            continue;
         }
     }
 
-    public function validateSpecifications(Schedule $schedule): bool
+    /**
+     * Avoid null pointer into the foreach.
+     */
+    public function scheduledDailyReport(): mixed
     {
-        for ($i = 1; $i <= 4; $i++) {
-            $specificationClass = self::REPORT_CLASS[$i];
-            $specification = new $specificationClass($schedule);
-            if ($specification->asQuery([]) === true) {
-                return true;
-            }
+        $schedules = Cache::get('scheduled-reports');
+        if ($schedules === null) {
+            Log::notice('No report where found to execute at current date ' . now()->toDateTimeString());
+            return [];
         }
-        return false;
+        return $schedules;
     }
 }
