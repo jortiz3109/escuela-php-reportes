de1eb5a29b3b987e1e12e42115164b937bcf028a
juanco <juancoloradoalvarez@gmail.com>
2021-11-02 13:08:46 -0500

    test: refactor SpecificationTest

--- tests/Unit/Scheduler/Specifications/SpecificationsTest.php
+++ tests/Unit/Scheduler/Specifications/SpecificationsTest.php
@@ -2,17 +2,15 @@
 
 namespace Tests\Unit\Scheduler\Specifications;
 
-use App\Constants\Schedule as ScheduleConstant;
-use App\Models\Report;
-use App\Models\Schedule;
 use App\Scheduler\Traits\SetCurrentDateTrait;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Support\Carbon;
 use Tests\Concerns\HasSchedulerProviders;
+use Tests\Helpers\SchedulerHelper;
 use Tests\TestCase;
 
 class SpecificationsTest extends TestCase
 {
+    use SchedulerHelper;
     use RefreshDatabase;
     use SetCurrentDateTrait;
     use HasSchedulerProviders;
@@ -25,11 +23,11 @@ class SpecificationsTest extends TestCase
      */
     public function specification_must_return_true_for_schedule_compatibility($specification, $schedule): void
     {
-        $knownDate = Carbon::create(2021, 10, 31, 10, 50);
-        Carbon::setTestNow($knownDate);
+        $this->setCurrentTime(2021, 10, 31, 10, 50);
+
         $schedule = $this->createSchedule($schedule);
         $hasMinutesHours = new $specification['specification_class']($schedule);
-        $this->assertTrue($hasMinutesHours->asQuery());
+        $this->assertTrue($hasMinutesHours->shouldExecuteReport());
     }
 
     /**
@@ -40,22 +38,9 @@ class SpecificationsTest extends TestCase
      */
     public function specification_must_return_false_for_not_schedule_compatibility($specification, $schedule): void
     {
-        $knownDate = Carbon::create(2021, 10, 31, 10, 50);
-        Carbon::setTestNow($knownDate);
+        $this->setCurrentTime(2021, 10, 31, 10, 50);
         $schedule = $this->createSchedule($schedule);
-        $hasMinutesHours = new $specification['specification_class']($schedule);
-        $this->assertFalse($hasMinutesHours->asQuery());
-    }
-
-    public function createSchedule(array $cronExpression): Schedule
-    {
-        return Schedule::create([
-            ScheduleConstant::MINUTE => $cronExpression[ScheduleConstant::MINUTE],
-            ScheduleConstant::HOUR => $cronExpression[ScheduleConstant::HOUR] ?: '*',
-            ScheduleConstant::DAY_MONTH => $cronExpression[ScheduleConstant::DAY_MONTH] ?: '*',
-            ScheduleConstant::MONTH => $cronExpression[ScheduleConstant::MONTH] ?: '*',
-            ScheduleConstant::DAY_WEEK => $cronExpression[ScheduleConstant::DAY_WEEK] ?: '*',
-            'report_id' => Report::create()->id,
-        ]);
+        $reportShouldBeRun = new $specification['specification_class']($schedule);
+        $this->assertFalse($reportShouldBeRun->shouldExecuteReport());
     }
 }
