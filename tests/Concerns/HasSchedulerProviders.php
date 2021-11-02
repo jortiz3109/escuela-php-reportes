a16a986c002d0967d579a6787b98a561b5b36d75
juanco <juancoloradoalvarez@gmail.com>
2021-11-02 13:13:51 -0500

    test: style cases of SchedulerProvider

--- tests/Concerns/HasSchedulerProviders.php
+++ tests/Concerns/HasSchedulerProviders.php
@@ -3,15 +3,14 @@
 namespace Tests\Concerns;
 
 use App\Constants\Schedule as ScheduleConstants;
-use App\Scheduler\Specifications\HasMinutesHours;
-use App\Scheduler\Specifications\HasMinutesHoursDays;
+use App\Scheduler\Specifications\CronReportShouldBeRun;
 
 trait HasSchedulerProviders
 {
     public function scheduledCronExpression(): array
     {
         return [
-            'all days at 8 am the report will send' => [
+            'all days at specific minute and hour a report will be execute' => [
                 'cronjob' => [
                     ScheduleConstants::MINUTE => 20,
                     ScheduleConstants::HOUR => 8,
@@ -27,7 +26,7 @@ trait HasSchedulerProviders
                     ScheduleConstants::MINUTE => 20, ],
             ],
 
-            'At every minute on day-of-month 9 a report will be execute' => [
+            'at specific day with hour and minute a report will be execute' => [
                 'cronjob' => [
                     ScheduleConstants::MINUTE => 20,
                     ScheduleConstants::HOUR => 20,
@@ -43,7 +42,7 @@ trait HasSchedulerProviders
                     ScheduleConstants::MINUTE => 20, ],
             ],
 
-            'At every minute in December a report will be execute' => [
+            'at specific month with day, hour and minute a report will be execute' => [
                 'cronjob' => [
                     ScheduleConstants::MINUTE => 20,
                     ScheduleConstants::HOUR => 8,
@@ -59,7 +58,7 @@ trait HasSchedulerProviders
                     ScheduleConstants::MINUTE => 20, ],
             ],
 
-            'At every minute on day-of-month 10 a report will be execute' => [
+            'at specific week day with month, day, hour and minute a report will be execute' => [
                 'cronjob' => [
                     ScheduleConstants::MINUTE => 20,
                     ScheduleConstants::HOUR => 8,
@@ -81,7 +80,7 @@ trait HasSchedulerProviders
     public function nonScheduledCronExpression(): array
     {
         return [
-            'all days at 8 am the report will send' => [
+            'all days at specific minute and hour a report will not be execute' => [
                 'cronjob' => [
                     ScheduleConstants::MINUTE => 20,
                     ScheduleConstants::HOUR => 7,
@@ -97,7 +96,7 @@ trait HasSchedulerProviders
                     ScheduleConstants::MINUTE => 20, ],
             ],
 
-            'At every minute on day-of-month 9 a report will be execute' => [
+            'at specific day with hour and minute a report will not be execute' => [
                 'cronjob' => [
                     ScheduleConstants::MINUTE => 20,
                     ScheduleConstants::HOUR => 20,
@@ -113,7 +112,7 @@ trait HasSchedulerProviders
                     ScheduleConstants::MINUTE => 20, ],
             ],
 
-            'At every minute in December a report will be execute' => [
+            'at specific month with day, hour and minute a report will not be execute' => [
                 'cronjob' => [
                     ScheduleConstants::MINUTE => 20,
                     ScheduleConstants::HOUR => 8,
@@ -129,62 +128,50 @@ trait HasSchedulerProviders
                     ScheduleConstants::MINUTE => 20, ],
             ],
 
-            'At every minute on day-of-month 10 a report will be execute' => [
+            'at specific week day with month, day, hour and minute a report will not be execute' => [
                 'cronjob' => [
                     ScheduleConstants::MINUTE => 20,
                     ScheduleConstants::HOUR => 8,
                     ScheduleConstants::DAY_MONTH => 22,
                     ScheduleConstants::MONTH => 10,
-                    ScheduleConstants::DAY_WEEK => 6,
+                    ScheduleConstants::DAY_WEEK => 5,
                 ],
                 'setTimeTest' => [
                     ScheduleConstants::YEAR => 2021,
                     ScheduleConstants::MONTH => 10,
                     ScheduleConstants::DAY_MONTH => 22,
                     ScheduleConstants::HOUR => 8,
-                    ScheduleConstants::MINUTE => 20,
+                    ScheduleConstants::MINUTE => 21,
                 ],
             ],
+
         ];
     }
 
-    public function schedulerSpecificationsSuccessTest()
+    public function schedulerSpecificationsSuccessTest(): array
     {
         return [
-            'Current specification compatibility HasMinutesHours' => [
+            'Current specification compatibility CronReportShouldBeRun' => [
                 'specification' => [
-                    'specification_class' => HasMinutesHours::class,
+                    'specification_class' => CronReportShouldBeRun::class,
                     ],
                 'schedule' => [
                     ScheduleConstants::MINUTE => 50,
                     ScheduleConstants::HOUR => 10,
-                    ScheduleConstants::DAY_MONTH => null,
-                    ScheduleConstants::MONTH => null,
-                    ScheduleConstants::DAY_WEEK => null,
-                ],
-            ],
-
-            'Current specification compatibility HasMinutesHoursDays' => [
-                'specification' => [
-                    'specification_class' => HasMinutesHoursDays::class,
-                ],
-                'schedule' => [
-                    ScheduleConstants::MINUTE => 50,
-                    ScheduleConstants::HOUR => 10,
-                    ScheduleConstants::DAY_MONTH => 31,
-                    ScheduleConstants::MONTH => null,
-                    ScheduleConstants::DAY_WEEK => null,
+                    ScheduleConstants::DAY_MONTH => '*',
+                    ScheduleConstants::MONTH => '*',
+                    ScheduleConstants::DAY_WEEK => '*',
                 ],
             ],
         ];
     }
 
-    public function schedulerSpecificationsFailTest()
+    public function schedulerSpecificationsFailTest(): array
     {
         return [
-            'Current specification compatibility HasMinutesHours' => [
+            'Current specification compatibility CronReportShouldBeRun' => [
                 'specification' => [
-                    'specification_class' => HasMinutesHours::class,
+                    'specification_class' => CronReportShouldBeRun::class,
                 ],
                 'schedule' => [
                     ScheduleConstants::MINUTE => 10,
@@ -195,18 +182,6 @@ trait HasSchedulerProviders
                 ],
             ],
 
-            'Current specification compatibility HasMinutesHoursDays' => [
-                'specification' => [
-                    'specification_class' => HasMinutesHoursDays::class,
-                ],
-                'schedule' => [
-                    ScheduleConstants::MINUTE => 50,
-                    ScheduleConstants::HOUR => 10,
-                    ScheduleConstants::DAY_MONTH => null,
-                    ScheduleConstants::MONTH => 30,
-                    ScheduleConstants::DAY_WEEK => null,
-                ],
-            ],
         ];
     }
 }
